5c768ba72e81e5fb6df352d2cf1c5bc830c8d77f
Marty McKnife <58376595+MartyMcKnife@users.noreply.github.com>
2021-01-27 17:48:37 +0800

    Fixed weird bug with searching

--- pages/api/photo/[user]/[label].ts
+++ pages/api/photo/[user]/[label].ts
@@ -3,39 +3,39 @@ import dbConnect from "../../../../utils/dbConnect";
 import { Photo } from "../../../../models/Photos";
 
 handler.get(async (req, res) => {
-  const {
+  let {
     query: { label, user },
   } = req;
 
   await dbConnect();
 
-  if (!label || !user) {
-    res
-      .status(400)
-      .json({ message: "Please provide a label and user to search for" });
-  } else {
-    await Photo.find(
-      { label: { $regex: `${label}`, $options: "i" }, user: user.toString() },
-      (err, docs) => {
-        if (err) {
-          res.status(503).json({
-            message: "Looks like mongoose had an unexpected error",
-            error: err,
-          });
-        } else {
-          if (docs.length <= 0) {
-            res.status(404).json({ message: "No images found" });
-          } else {
-            const data = docs.map((doc) => {
-              return { url: doc.url, label: doc.label };
-            });
+  if (!label) {
+    console.log("no label");
+    label = "";
+  }
 
-            res.status(200).json(data);
-          }
+  console.log(label);
+  await Photo.find(
+    { label: { $regex: `${label}`, $options: "i" }, user: user.toString() },
+    (err, docs) => {
+      if (err) {
+        res.status(503).json({
+          message: "Looks like mongoose had an unexpected error",
+          error: err,
+        });
+      } else {
+        if (docs.length <= 0) {
+          res.status(404).json({ message: "No images found" });
+        } else {
+          const data = docs.map((doc) => {
+            return { url: doc.url, label: doc.label };
+          });
+
+          res.status(200).json(data);
         }
       }
-    );
-  }
+    }
+  );
 });
 
 handler.delete(async (req, res) => {
